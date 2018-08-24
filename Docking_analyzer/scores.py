265a40331a996e3d977a156d7aaf21baf40691b2
AngelRuizMoreno <angel.j.ruiz.moreno@gmail.com>
2018-08-24 11:58:36 -0500

    improving the library

--- Docking_analyzer/scores.py
+++ Docking_analyzer/scores.py
@@ -12,36 +12,56 @@ from matplotlib import pyplot as plt
 
 
 # NOW TO THE FUNTIONS
-def get_scores (self,path=None,docker=None, save_csv=True, sort=True):
-    path    = None
-    docker  = None
-    save_csv= True
-    sort    =True
-    if docker == 'autodockvina':
-        os.chdir (path) #Path where *.pdbqt output files are located
-        files=[]
-        scores=[]
-        for file in glob.glob('*.pdbqt'):
-            with open(file,'rt') as file:
-                for line in file:
-                    line = line.strip()
-                    if "VINA RESULT" in line:
-                    neg = re.search(r'-\d.\d', line)
-                    if neg:
-                        files.append (file.name)
-                        scores.append (neg.group())
-        d={'file':pd.Series(files),'score':pd.Series(scores)}
-        self.table=pd.DataFrame (d)
-        print (table)
+class get_scores ():
 
+    def __init__ (self,path=None,docker=None, save_csv=True, sort=True):
+        self.path       = path
+        self.docker     = docker
+        self.save_csv   = save_csv
+        self.sort       = sort
+        if docker == 'autodockvina':
+            os.chdir (path) #Path where *.pdbqt output files are located
+            files=[]
+            scores=[]
+            for file in glob.glob('*.pdbqt'):
+                with open(file,'rt') as file:
+                    for line in file:
+                        line = line.strip()
+                        if "VINA RESULT" in line:
+                            neg = re.search(r'-\d.\d', line)
+                            if neg:
+                                files.append (file.name)
+                                scores.append (neg.group())
+            d={'file':pd.Series(files),'score':pd.Series(scores)}
+            print ('Raw score values')
+            table=pd.DataFrame (d)
+            print (table)
+
+        pass
+
+        if docker== 'gold':
+            os.chdir (path) #Path where *.pdbqt output files are located
+            files=[]
+            scores=[]
+            for file in glob.glob('*.mol2'):
+                with open(file,'rt') as file:
+                    for line in file:
+                        line = line.strip()
+                        if "<score>" in line:
+                            neg = re.search(r'\d\d.\d\d', line)
+                            if neg:
+                                files.append (file.name)
+                                scores.append (neg.group())
+            d={'file':pd.Series(files),'score':pd.Series(scores)}
+            print ('Raw score values')
+            table=pd.DataFrame (d)
+            print (table)
+        pass
+
+        if sort == True:
+            print ('Sorted score values')
+            sort=table.sort_values ('score',ascending=False)
+            print (sort)
         if save_csv == True:
             table.to_csv ('no_sorted_scores.csv')
-        if sort == True:
-            sort=table.sort_values ('score',ascending=False)
             sort.to_csv ('sorted_scores.csv')
-        pass
-    pass
-
-    if docker == 'gold':
-        pass
-    pass
